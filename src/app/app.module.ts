7e01da77de8fa6680eef65eda0aae93367209496
oleg.bezkorovaynyi <oleg.bezkororvaynyi@globallogic.com>
2017-05-21 21:43:14 +0300

    configure routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,11 +12,17 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const appRoutes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'users', component: UsersComponent},
-  {path: 'servers', component: ServersComponent}
+  {path: 'users', component: UsersComponent, children: [
+    {path: ':id/:name', component: UserComponent}
+  ]},
+  {path: 'servers', component: ServersComponent, children: [
+    {path: ':id', component: ServerComponent},
+    {path: ':id/edit', component: EditServerComponent}
+  ]},
 ];
 
 @NgModule({
@@ -27,7 +33,8 @@ const appRoutes: Routes = [
     ServersComponent,
     UserComponent,
     EditServerComponent,
-    ServerComponent
+    ServerComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
